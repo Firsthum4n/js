ba038d91f09475e50d02eb06758237c086ce2d43
Zan <zanfilipp259@gmail.com>
2024-08-11 21:01:43 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+// var stroka = 'word';
+
+// switch(stroka) {
+//     case "4": 
+//     console.log("value 4");
+//     break;
+//     default:
+//         console.log('default');
+// }
+
+// switch(stroka) {
+//     case "word": 
+//     console.log("value 4");
+//     break;
+// }
+
+
+
+// var number = 15;
+// var isHasHouse = true
+
+// if(number == 15 && isHasHouse == true) {
+//     console.log('ok');
+// } else if (number < 10) {
+//     console.log('ok!');
+// } else if (number ==7 ) {
+//     console.log('7');
+// } else if (number >=15) {
+//     console.log('>=15');
+// }else {
+//     console.log('not ok');
+// }
