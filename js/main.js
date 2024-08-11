63c69539f5cbd9ce424b087ad74461a47b4bad58
Zan <zanfilipp259@gmail.com>
2024-08-12 00:53:43 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,72 @@
+alert('hi');
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+// var arr = [5, 7, 3, 8, 9, 'stroka'];
+// for(var i = 0; i < arr.length; i++) {
+//     console.log('element ' + (i + 1) + ': ' + arr[i])
+// }
+
+// for(var i = 10; i <= 20; i++) {
+//     if(i % 2 == 0)
+//         continue;
+    
+//     console.log(i)
+// }
+
+// for(var i = 10; i <= 20; i += 2) {
+//     if(i > 15)
+//         break;
+
+//     console.log(i)
+// }
+
+
+// var x = 0;
+// do {
+//     console.log(x);
+//     x++;
+// } while(x < 10);
+
+// var j = 1000;
+// while(j >= 100) {
+//     console.log(j);
+//     j-=100;
+// }
+
+// var isHasCar = true;
+// while(isHasCar) {
+// }
+
+// for (var i = 100; i > 5; i /=2) {
+//     console.log(i)
+// }
+
+
+// var arr = [5, true, 'stroka', 5.7, 0, -100];
+// arr[3] = 'word'
+// console.log(arr);
+
+// var matrix = 
+//   [[4, 6, 8], ['stroka', 5.7], [0, -100]
+// ];
+
+// console.log(matrix[1][0])
+// matrix[1][0] = 90
+// console.log(matrix)
 
 
 
